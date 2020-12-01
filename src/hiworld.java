baf684b114ab8bf25602fa058e91da0e4be8dd23
贝贝 <1368698567@qq.com>
2020-12-01 12:46:05 +0800

    test

--- src/hiworld.java
+++ src/hiworld.java
@@ -1,6 +1,6 @@
 public class hiworld {
     public static void main(String [] args){
         System.out.println("hi Rebeccazheng!");
-        System.out.println("hi chenmingxong!");
+        System.out.println("hi chenmingcong!");
     }
 }
